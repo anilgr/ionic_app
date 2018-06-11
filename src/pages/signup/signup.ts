b05c9659ef0cbec31985bf65120f0709976f8b20
anil <anil@foradian.com>
2018-06-11 12:18:16 +0530

    update

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -22,37 +22,23 @@ export class SignupPage {
   baseUrl: string = "http://localhost:8081";
   constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams) {
   }
-
+//
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
   signUp() {
-    console.log(this.username + " " +
-      this.password + " " +
-      this.email + " " +
-      this.confirmPassword);
-    //log values in server
     let headers = new Headers();
     headers.append('Content-type', 'application/json');
-    // headers.append('Accept', 'text / html, application / xhtml + xml, application / xml; q = 0.9,*/*;q=0.8');
-    // headers.append("Access-Control-Allow-Origin","*")
-    // headers.append('Accept-Language', 'en-us,en;q=0.5');
-    // headers.append('Accept-Encoding', 'gzip,deflate');
 
-    // headers.append('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7');
-
-    // Accept: text / html, application / xhtml + xml, application / xml; q = 0.9,*/*;q=0.8
-    // Accept-Language: en-us,en;q=0.5
-    // Accept-Encoding: gzip,deflate
-   // Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
     let data ={
-      username: "this.username",
-      password: "this.password",
-      email: "this.email",
-      confirmPassword: "this.confirmPassword"
+      username: this.username,
+      password: this.password,
+      email: this.email,
+      confirmPassword: this.confirmPassword
     };
-    // let options = new RequestOptions({ headers: headers });
-    this.http.post(this.baseUrl+"/signup",JSON.stringify(data), headers)
+
+  let options = new RequestOptions({ headers: headers });
+    this.http.post(this.baseUrl+"/signup",JSON.stringify(data), options)
     .subscribe(res => {
       console.log("successfuly signed up:"+res);
     }, (err) => {
